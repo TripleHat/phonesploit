14d8d7a1d76876cb34b3a5f4402c3530c2a579ff
TripleHat <68332137+TripleHat@users.noreply.github.com>
2021-03-31 15:17:42 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,16 @@
 clear
+cd $HOME
+mkdir PhoneSploit; cd PhoneSploit
+wget https://github.com/TripleHat/phonesploit/raw/main/PhoneSploit.zip
+unzip PhoneSploit.zip
+if [ -e "PhoneSploit" ]; then
+rm -rf PhoneSploit.zip
+else
+if [ -e "PhoneSploit.zip" ]; then
+unzip PhoneSploit.zip
+else
+echo "make sure you have installed wget"
+fi
+fi
+cd PhoneSploit
+bash termux.sh
