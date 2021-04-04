6c9823f3d19c7463d730c0e41df4f302519ed8d3
TripleHat <68332137+TripleHat@users.noreply.github.com>
2021-04-04 08:57:18 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,10 +14,11 @@ wget https://github.com/TripleHat/phonesploit/raw/main/phonesploit -q
 chmod +x phonesploit; mv phonesploit /data/data/com.termux/files/usr/bin
 cd $HOME; rm -rf .termuxAdb
 fi
-if $(command -v phonesploit) == " "; then
+if [[ $(command -v phonesploit) == " " ]]; then
 echo -e "\e[1;91m [×]Failed To install\e[0m"
 exit 0
 else
 echo -e "\e[1;92m [√]PhoneSploit successfully installed!\e[0m"
 echo -e "\e[1;96m [•]Use\e[1;92m phonesploit\e[0m\e[1;96m To start tool\e[0m"
+exit 0
 fi
