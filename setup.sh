cfb39affdcceddb408dbc39bed3101d37fb564c0
TripleHat <68332137+TripleHat@users.noreply.github.com>
2021-04-04 05:30:39 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,22 @@
 clear
 cd $HOME
+echo -e "\e[1;92m [~]Installing phonesploit\e[0m"
+sleep 1
 if -e -d ".termuxAdb"; then
 cd .termuxAdb; rm -rf /*
 wget https://github.com/TripleHat/phonesploit/raw/main/phonesploit -q
 chmod +x phonesploit; mv phonesploit /data/data/com.termux/files/usr/bin
+cd $HOME; rm -rf .termuxAdb
 else
 mkdir .termuxAdb; cd .termuxAdb
 wget https://github.com/TripleHat/phonesploit/raw/main/phonesploit -q
 chmod +x phonesploit; mv phonesploit /data/data/com.termux/files/usr/bin
+cd $HOME; rm -rf .termuxAdb
 fi
-if [ -e "PhoneSploit" ]; then
-rm -rf PhoneSploit.zip
+if $(command -v phonesploit) == " "; then
+echo -e "\e[1;91m [×]Failed To install\e[0m"
+exit 0
 else
-if [ -e "PhoneSploit.zip" ]; then
-unzip PhoneSploit.zip
-else
-echo "make sure you have installed wget"
+echo -e "\e[1;92m [√]PhoneSploit successfully installed!\e[0m"
+echo -e "\e[1;96m [•]Use\e[1;92m phonesploit\e[0m\e[1;96m To start tool\e[0m"
 fi
-fi
-cd PhoneSploit
-chmod +x termux.sh
-bash termux.sh
